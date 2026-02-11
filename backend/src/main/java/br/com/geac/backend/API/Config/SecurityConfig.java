5e9712ed9f169bb6631e91fddfacc9c7d2e6c876
Dimas Neto <dimasneto2015@gmail.com>
2026-02-11 13:31:01 -0300

    add cors

--- backend/src/main/java/br/com/geac/backend/API/Config/SecurityConfig.java
+++ backend/src/main/java/br/com/geac/backend/API/Config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors()
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
